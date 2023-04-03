32128e369c3a8f207805b5ea01ddf1c94112708d
Jamesishaku <jamezishaku@gmail.com>
2023-04-03 14:18:29 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,13 +15,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	if (size > 0)
 	{
-		int i = 0;
+		int i;
 
-		while(i < size)
-		{
+		for (i = 0; i < size; i++)
 			dest[i] = src[i];
-			i++;
-		}
 	}
 
 	return (dest);
