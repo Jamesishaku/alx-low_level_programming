488ce3103c48ebb7502733b1003e495d14398a15
Jamesishaku <jamezishaku@gmail.com>
2023-05-01 13:11:44 -0700

    fixed indentation issue

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -19,8 +19,8 @@ size_t print_listint(const listint_t *h)
 		else
 		{
 			printf("%d\n", h->n);
-                	h = h->next;
-                	nnodes++;
+			h = h->next;
+			nnodes++;
 		}
 	}
 	return (nnodes);
