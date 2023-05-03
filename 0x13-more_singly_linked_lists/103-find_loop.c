15adf18ab1df40e8261636107f95d168cf5e7377
Jamesishaku <jamezishaku@gmail.com>
2023-05-03 14:00:05 -0700

    Find the loop - fix white spacing issue

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,25 +10,25 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *turtle = head, *hare = head;
-	
+
 	while (turtle != NULL && hare != NULL && hare->next != NULL)
 	{
 		turtle = turtle->next;
 		hare = hare->next->next;
-		
+
 		if (turtle == hare)
 		{
 			turtle = head;
-		       
+
 			while (turtle != hare)
 			{
 				turtle = turtle->next;
 				hare = hare->next;
 			}
-			
+
 			return (turtle);
 		}
 	}
-	
+
 	return (NULL);
 }
