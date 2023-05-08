0ffd076a7e639119bf83b6b2b68b79815e667339
Jamesishaku <jamezishaku@gmail.com>
2023-05-08 15:08:35 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,21 +18,18 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	input_fd = open(argv[1], O_RDONLY);
 	if (input_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (output_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((n_bytes_read = read(input_fd, text, BUF_SIZE)) > 0)
 	{
 		n_bytes_write = write(output_fd, text, n_bytes_read);
@@ -42,13 +39,11 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (n_bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	close_file(input_fd);
 	close_file(output_fd);
 
